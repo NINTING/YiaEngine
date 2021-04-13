1b64f9a6cddcadbb4d87f89773874e0d8cff1652
NINTING <1075364860@qq.com>
2021-04-13 23:26:39 +0800

    empty artical

---
+++ YiaEngine/Engine/Framework/Common/YiaEngine.cpp
@@ -0,0 +1,16 @@
+﻿// YiaEngine.cpp: 定义应用程序的入口点。
+//
+
+#include "YiaEngine.h"
+#include "Interface.h"
+#include "Application.h"
+using namespace std;
+
+int main()
+{
+	cout << "Fuck CMake。" << endl;
+	printf("dssas");
+	f();
+	app();
+	return 0;
+}
---
+++ YiaEngine/Engine/Framework/Interface/Interface.h
@@ -0,0 +1,13 @@
+﻿// YiaEngine.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+
+void f()
+{
+	printf("interface");
+}
+
+// TODO: 在此处引用程序需要的其他标头。
